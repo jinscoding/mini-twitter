1ddefa40b7d45ef108966e5ebf0903dd9d6afa75
jinscoding <92193087+jinscoding@users.noreply.github.com>
2024-06-12 18:04:41 +0900

    트윗 렌더링

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
 import PostTweetForm from "../components/PostTweetForm";
+import Timeline from "../components/Timeline";
 
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  overflow-y: scroll;
+  grid-template-rows: 1fr 5fr;
+`;
 export default function Home() {
-  return <PostTweetForm />;
+  return (
+    <Wrapper>
+      <PostTweetForm />
+      <Timeline />
+    </Wrapper>
+  );
 }
